e670df2168f56560391102cbdfc2818c8b079aff
Matt Webb <matt@interconnected.org>
2024-01-22 13:28:24 +0000

    Page furniture

--- templates/chat-room/src/App.tsx
+++ templates/chat-room/src/App.tsx
@@ -1,18 +1,24 @@
 import Counter from "./Counter";
 import ChatRoom from "./ChatRoom";
+import Footer from "./Footer";
+import Header from "./Header";
 
 function App() {
   return (
-    <main
-      className="w-full max-w-4xl mx-auto p-4 h-full flex flex-col gap-4 justify-start"
+    <div
+      className="w-full max-w-4xl mx-auto p-4 gap-6 bg-white flex flex-col justify-between items-between"
       style={{ minHeight: "100dvh" }}
     >
-      <h1 className="text-3xl font-semibold text-blue-500">My AI Chat Room</h1>
-      <ChatRoom
-        host={process.env.REACT_APP_PARTYKIT_HOST as string}
-        roomName="default-room"
-      />
-    </main>
+      <Header />
+      <main className="grow w-full h-full flex flex-col gap-6 justify-start items-start">
+        <h1 className="text-3xl font-semibold text-blue-500">AI Chat Room</h1>
+        <ChatRoom
+          host={process.env.REACT_APP_PARTYKIT_HOST as string}
+          roomName="default-room"
+        />
+      </main>
+      <Footer />
+    </div>
   );
 }
 
