fa332d5578659b4d298c31374213bf8e1991a8f7
Matt Webb <matt@interconnected.org>
2024-01-22 10:51:12 +0000

    React with Tailwind

---
+++ templates/chat-room/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="w-full max-w-4xl mx-auto p-4">
+      <h1 className="text-3xl font-semibold text-blue-500">Hello, World!</h1>
+    </main>
+  );
+}
+
+export default App;
